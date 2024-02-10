6d7b5bde434b632c4d747f9a704895761aaadeff
Sylvain Vallot <29737056+sylvainvallot@users.noreply.github.com>
2024-02-10 10:37:22 +0100

    add link to icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Logo from "../public/images/lnac-logo.svg";
 import PlatonHalf from "../public/images/platon-half.svg";
-import { FooterLinks } from "@/lib/navigation.js";
+import { FooterLinks, Links } from "@/lib/navigation.js";
 
 export default function Footer() {
 	return (
@@ -21,7 +21,7 @@ export default function Footer() {
 						))}
 					</nav>
 					<div className="mt-8 flex justify-center space-x-6">
-						<a href="#" className="text-gray-400 hover:text-gray-600">
+						<a href={Links.Twitter} className="text-gray-400 hover:text-gray-600">
 							<span className="sr-only">Twitter</span>
 							<svg
 								className="h-6 w-6"
@@ -32,7 +32,7 @@ export default function Footer() {
 								<path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
 							</svg>
 						</a>
-						{/* <a href="#" className="text-gray-400 hover:text-gray-600">
+						<a href={Links.GitHub} className="text-gray-400 hover:text-gray-600">
 							<span className="sr-only">GitHub</span>
 							<svg
 								className="h-6 w-6"
@@ -46,8 +46,8 @@ export default function Footer() {
 									clipRule="evenodd"
 								></path>
 							</svg>
-						</a> */}
-						<a href="#" className="text-gray-400 hover:text-gray-600">
+						</a>
+						<a href={Links.DiscordInvite} className="text-gray-400 hover:text-gray-600">
 							<span className="sr-only">Discord</span>
 							<svg
 								className="h-6 w-6"
