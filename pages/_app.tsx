0e83fabc03409a54134d26f07301966addb13d8d
Sylvain Vallot <29737056+sylvainvallot@users.noreply.github.com>
2024-02-10 11:04:29 +0100

    add icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 			{/* <html className="fixed -z-50 h-screen w-full bg-gradient-to-br from-blue-50 via-white to-blue-100"></html> */}
 			<Head>
 				<title>La Nouvelle Académie</title>
+				<meta name="description" content="La Nouvelle Académie est un serveur discord d'aide aux devoirs et d'entraide scolaire."/>
+				<link rel="icon" href="/favicon-96x96.png"/>
 			</Head>
 			<NavBar />
 			<div className="pt-32">
